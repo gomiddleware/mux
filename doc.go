13621e18ba3462f2ddabeea0f13c071b69c31d9e
Andrew Chilton <andychilton@gmail.com>
2017-01-26 23:54:26 +1300

    More docs

--- doc.go
+++ doc.go
@@ -1,32 +1,61 @@
-// Package mux is a simple-mux, however, it provides a few nice features such as easy use of middleware and a router
-// which doesn't automatically look at only prefixes (like the Go built-in mux).
+// Package mux is a simple(ish) mux, however, it provides a few nice features such as easy use of middleware and a
+// router which doesn't automatically look at only prefixes (like the Go built-in mux). Everything is explicit and
+// nothing is done for you. ie. we don't automatically redirec to a slash or non-slash version of each path, you need
+// to do that yourself (or use github.com/gomiddleware/slash to help).
 //
-// Middleware can be added to both prefixes and to endpoints, which means that the simple composability of this gives a
-// lot of power. Inclusion of a middleware pipeline within the router also provides lots of power, plus less end-user
-// code.
+// There are two fundamentals at work here, middleware and handlers. Middleware is expected to either: (1) do something
+// with the request/response/context on the way in, call the `next` middleware and optionally do something on the way
+// out, or (2) deal with the request completely and not call the `next` middleware at all. It is up to the middleware to decide this.
+//
+// An example of middleware calling next might be adding a requestId in to each request's Context. An
+//
+// An example of middleware dealing with the request might be to check that a user is logged in, and if not, sending a
+// redirect to "/", or sending back a 403.
+//
+// For the purposes of this library (and in general), a middleware is defined as one of these:
+//
+// • func(http.Handler) http.Handler
+//
+// • func(http.HandlerFunc) http.HandlerFunc
+//
+// For the purposes of this library (and in general), a handler is defined as:
+//
+// • http.Handler
+//
+// • func(http.ResponseWriter, *http.Request)
+//
+// To add middleware to any prefix, use:
+//
+//     m.Use("/prefix", middlewares...)
+//
+// To add a handler to any path, use one of Get/Post/Put/Patch/Delete/Options/Head such as:
+//
+//     m.Get("/path", [middlewares...,] handler)
+//
+// Of course, you can only add one handler to each route, and it must be the last thing you pass in.
+//
+// Don't worry that you are passing one of 4 types in to each method since they are organised at the time they are
+// added, and not during the request cycle. Only type assertions are used and not reflection.
+//
+// Example
 //
 //     m := mux.New()
+//
+//     m.Use("/", logger.New())
 //
 //     m.Get("/", func(w http.ResponseWriter, r *http.Request) {
 //         w.WriteHeader(http.StatusOK)
 //         w.Write([]byte("Home\n"))
 //     })
 //
+//     m.Get("/my/", checkUserIsSignedIn, userHomeHandler)
+//
+//     if m.Err != nil {
+//         // ... something went wrong with a route
+//     }
+//
 //     log.Fatal(http.ListenAndServe(":8080", m))
 //
-// Inspired by a few other routers and middlewares such as:
-//
-//     • https://github.com/justinas/alice
-//     • https://golang.org/pkg/net/http/#ServeMux
-//     • https://github.com/gorilla/mux
-//     • https://github.com/julienschmidt/httprouter
-//     • https://github.com/pressly/chi
-//     • https://github.com/dimfeld/httptreemux
-//     • https://github.com/bmizerany/pat
-//     • https://github.com/pilu/traffic
-//     • https://github.com/rcrowley/go-tigertonic
-//     • https://github.com/mikespook/possum
-//     • https://github.com/zenazn/goji/
-//     • https://github.com/gocraft/web
+// More information and examples can be found at http://chilts.org/2017/01/27/gomiddleware-mux
 //
 package mux
